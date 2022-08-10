e0ad25dc8547e41aa0ce48a99cb1fd00ffa52102
Elenilson <elenilsonsantos9@gmail.com>
2022-08-09 22:46:28 -0300

    exercicios resolvidos

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,9 +1,28 @@
-let idade;
-let isMaiorIdade;
-let isCursandoOutraFaculdade;
+// # Exercício 2
 
-if(){
+// Escreva um código que receba três valores. Uma idade, um booleano que responda se a pessoa terminou ou não o ensino médio, e um booleano que responda se a pessoa está cursando alguma faculdade.
 
+let idade = 27
+let terminouEnsinoMedio = true
+let cursandoAlgumaFaculdade = false
+
+// - Se a pessoa tem 18 anos ou mais;
+if(idade >=18){
+console.log('voce é maior de idade')
+}else if(idade> 0 && idade <=17){
+	console.log('voce é menor de idade')
 }else{
-	
+    alert('dados inválidos: digite um valor válido.')
+}
+// - Se a pessoa terminou o ensino médio;
+if(terminouEnsinoMedio === true){
+console.log('você concluiu o ensino médio. Parabéns!!')
+}else if(terminouEnsinoMedio === false){
+console.log('você não concluiu o ensino médio.tente novamente, você consegue!!')
+}
+// - Se a pessoa NÃO está cursando alguma faculdade;
+if(cursandoAlgumaFaculdade === false){
+    console.log('voce cursa uma faculdade')
+}else if(cursandoAlgumaFaculdade === true){
+console.log('voce não cursa uma faculdade')
 }
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,45 @@
-let nacionalidade //= adicione o prompt aqui
+// # Exercício 3
+
+// Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const `nacionalidade`.
+
+// Vamos considerar que nosso programa aceita as seguintes 5 nacionalidades:
+// - brasileira;
+// - argentina;
+// - uruguaia;
+// - chilena;
+// - colombiana;
+
+
+
+// > 💡 Lembre-se de prestar atenção às letras maiúsculas e minúsculas. 
+
+
+//  Crie um código que receba um **prompt** que diga: "Escreva aqui sua nacionalidade" e guarde o valor da resposta em uma const `nacionalidade`.
+let nacionalidade = prompt('Escreva sua nacionalidade aqui!').toLowerCase()
+const nacionalidade1 = nacionalidade
+
+// Crie uma estrutura de `if/else if/else` que verifique se a nacionalidade guardada na const `nacionalidade` é igual a cada uma das nacionalidades acima, e caso seja, imprima a nacionalidade no console. O último `else` deve imprimir "nacionalidade não encontrada" caso o valor de `nacionalidade` não corresponda a nenhum dos valores.
+if(nacionalidade1 === 'brasileira' ){
+    alert('nacionalidade: brasileira(o), nascido no brasil!')
+    console.log('nacionalidade: brasileira(o)')
+    
+}else if(nacionalidade1 === 'argentino'|| nacionalidade1 === 'argentina'){
+    alert('nacionalidade: argentino(a), nascido na argentina!')
+    console.log('nacionalidade: argentino(a)')
+   
+}else if(nacionalidade1 === 'uruguaia'|| nacionalidade1 === 'uruguaio'){
+    alert('nacionalidade: urugiaia(o), nascido no uruguai!')
+    console.log('nacionalidade: urugiaia(o)')
+
+}else if(nacionalidade1 === 'chilena'|| nacionalidade1 === 'chileno'){
+    alert('nacionalidade: chilena(o), nascido no chile!')
+    console.log('nacionalidade: chilena(o)')
+
+}else if(nacionalidade1 === 'colombiano'|| nacionalidade1 === 'colombiana'){
+   alert('nacionalidade: colombiano(a), nascido na colômbia!')
+    console.log('nacionalidade: colombiano(a)')
+
+}else{
+   alert('nacionalidade não encontrada.')
+    console.log('nacionalidade não encontrada')
+}
